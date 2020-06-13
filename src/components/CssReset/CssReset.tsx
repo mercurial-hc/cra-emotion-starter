5a3335c7838d0d384c1dd80b661c453ae832f2e7
mercurial-hc <45140148+mercurial-hc@users.noreply.github.com>
2020-06-13 20:58:58 +0530

    Update CssReset.tsx

--- src/components/CssReset/CssReset.tsx
+++ src/components/CssReset/CssReset.tsx
@@ -18,14 +18,6 @@ const CssReset: React.FC = () => (
       *::after {
         box-sizing: border-box;
       }
-
-      body {
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
-          "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
-          "Helvetica Neue", sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-      }
     `}
   />
 );
