fc98001e24839cf7ab691e431de5ed416f3a8da8
t-dambm <mahendra.dambe@autodesk.com>
2020-06-21 14:12:12 +0530

    - regenerate yarn.lock - add dev-dependency cross-env - update scripts for portability

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import { store } from "services";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders main content", () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
